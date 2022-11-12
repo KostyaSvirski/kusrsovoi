8ca0d3c0d5418bc08b6bf8f19c14563d10a70603
Kostya svirski <kostyashtc@gmail.com>
2022-11-12 16:41:55 +0300

    [kursovoi] Refactor injection of commandHolder

--- src/main/java/org/kursovoi/server/controller/RequestDispatcher.java
+++ src/main/java/org/kursovoi/server/controller/RequestDispatcher.java
@@ -1,14 +1,16 @@
 package org.kursovoi.server.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.kursovoi.server.controller.command.Command;
 import org.kursovoi.server.controller.command.CommandHolder;
 import org.kursovoi.server.util.exception.UnknownCommandException;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class RequestDispatcher {
 
-    private final CommandHolder commandHolder = CommandHolder.getInstance();
+    private final CommandHolder commandHolder;
 
     public Command matchCommand(String nameOfCommand) {
         var commands = commandHolder.getCommands();
